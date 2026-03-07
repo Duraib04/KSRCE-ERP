5345947b7683db3e8268c6434e5d9c737c29ac08
Durai <itsdurai4@gmail.com>
2026-03-07 11:52:36 +0530

    Update flutter_bootstrap.js and pubspec.lock

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"ab3b766230bfccb37754dd5a28a61b31a7c9bc
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4246888296" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "843050499" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
